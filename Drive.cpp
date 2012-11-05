0c241be7b5d723ad7f79afc2dc3a9f32ada1d703
Jeremy Dalton <blackwolfe@hotmail.co.uk>
2012-11-05 23:36:16 +0000

    linear drive code fleshed out
    requires code to write to outputs adding in

--- Drive.cpp
+++ Drive.cpp
@@ -12,11 +12,11 @@
 
 #define rampDelay	10	 // Delay between speed step changes
 #define RMclkPin	2	// Right Motor clock pin
-#define RMdirPin	3	// Right motor direction pin
-#define RMprePin	4	// Right Motor preset pin
-#define LMclkPin	5	// Left Motor clock pin
-#define LMdirPin	6	// Left Motor direction pin
-#define LMprePin	7	// Left Motor preset pin
+#define RMdirPin	42	// Right motor direction pin
+#define RMprePin	43	// Right Motor preset pin
+#define LMclkPin	3	// Left Motor clock pin
+#define LMdirPin	44	// Left Motor direction pin
+#define LMprePin	45	// Left Motor preset pin
 
 Drive::Drive() {
 
@@ -42,23 +42,60 @@ void Drive::init(int verbose) {
 void Drive::forward(int debug, int motor, int speed) {
 	//Motor select options:	1=> Both only	2=> Right only	3=> Left only
 
-	//Each case tests current speed vs desired speed then call
-	//then appropriate ramp function to change speed.
-
-	// set direction to forwards
+	//-> Check direction is set to forwards
+	//-> test current speed vs desired speed then call appropriate ramp function to change speed.
 
 	switch (motor) {
 	case '1':
+		if (RMdir == 0) {
+			RMdir = 1;
+			//digital write direction to pin
+		}
+		if (LMdir == 0) {
+			RMdir = 1;
+			//digital write direction to pin
+		}
 
+		if (speed > BMspe) {
+			rampUp(speed, motor);
+		}
+		else if (speed < BMspe) {
+			rampDown(speed, motor);
+		}
+		else {
+		}
 		break;
+
 	case '2':
+		if (RMdir == 0) {
+			RMdir = 1;
+			//digital write direction to pin
+		}
 
+		if (speed > BMspe) {
+			rampUp(speed, motor);
+		}
+		else if (speed < BMspe) {
+			rampDown(speed, motor);
+		}
+		else {
+		}
 		break;
+
 	case '3':
+		if (LMdir == 0) {
+			LMdir = 1;
+			//digital write direction to pin
+		}
 
-		break;
-	default:
-
+		if (speed > BMspe) {
+			rampUp(speed, motor);
+		}
+		else if (speed < BMspe) {
+			rampDown(speed, motor);
+		}
+		else {
+		}
 		break;
 	}
 }
@@ -66,26 +103,65 @@ void Drive::forward(int debug, int motor, int speed) {
 void Drive::backward(int debug, int motor, int speed) {
 	//Motor select options:	1=> Both only	2=> Right only	3=> Left only
 
-	//Set direction to backwards
+	//-> Check direction is set to forwards
+	//-> Each case tests current speed vs desired speed then calls then appropriate ramp function to change speed.
 
 	switch (motor) {
 	case '1':
+		if (RMdir == 1) {
+			RMdir = 0;
+			//digital write direction to pin
+		}
+		if (LMdir == 1) {
+			RMdir = 0;
+			//digital write direction to pin
+		}
 
+		if (speed > BMspe) {
+			rampUp(speed, motor);
+		}
+		else if (speed < BMspe) {
+			rampDown(speed, motor);
+		}
+		else {
+		}
 		break;
+
 	case '2':
+		if (RMdir == 1) {
+			RMdir = 0;
+			//digital write direction to pin
+		}
 
+		if (speed > BMspe) {
+			rampUp(speed, motor);
+		}
+		else if (speed < BMspe) {
+			rampDown(speed, motor);
+		}
+		else {
+		}
 		break;
+
 	case '3':
+		if (LMdir == 1) {
+			LMdir = 0;
+			//digital write direction to pin
+		}
 
-		break;
-	default:
-
+		if (speed > BMspe) {
+			rampUp(speed, motor);
+		}
+		else if (speed < BMspe) {
+			rampDown(speed, motor);
+		}
+		else {
+		}
 		break;
 	}
-
 }
 
-void Drive::stop(int debug, int motor) {
+void Drive::stop(int debug, int motor = 1) {
 	rampDown(0, motor);
 };
 
@@ -93,16 +169,26 @@ void Drive::rampUp(int maxSpeed, int motor) {
 	//Motor select options:	1=> Both only	2=> Right only	3=> Left only	
 	switch (motor) {
 	case '1':
-
+		for (int i = BMspe; i <= maxSpeed; i++) {
+			//write 'i' to output
+		}
+		//sync new speed to BMspe
+		//sync new speed to RMspe
+		//sync new speed to LMspe
 		break;
+
 	case '2':
-
+		for (int i = RMspe; i <= maxSpeed; i++) {
+			//write 'i' to output
+		}
+		//sync new speed to RMspe
 		break;
+
 	case '3':
-
-		break;
-	default:
-
+		for (int i = LMspe; i <= maxSpeed; i++) {
+			//write 'i' to output
+		}
+		//sync new speed to LMspe
 		break;
 	}
 }
@@ -111,16 +197,26 @@ void Drive::rampDown(int minSpeed, int motor) {
 	//Motor select options:	1=> Both only	2=> Right only	3=> Left only
 	switch (motor) {
 	case '1':
-
+		for (int i = BMspe; i >= minSpeed; i--) {
+			//write 'i' to output
+		}
+		//sync new speed to BMspe
+		//sync new speed to RMspe
+		//sync new speed to LMspe
 		break;
+
 	case '2':
-
+		for (int i = RMspe; i >= minSpeed; i--) {
+			//write 'i' to output
+		}
+		//sync new speed to RMspe
 		break;
+
 	case '3':
-
-		break;
-	default:
-
+		for (int i = LMspe; i >= minSpeed; i--) {
+			//write 'i' to output
+		}
+		//sync new speed to LMspe
 		break;
 	}
 }
